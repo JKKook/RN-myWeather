7e074d76e61095fbaad4cbb7311b157ab8aa3577
JKKook <whdrnrdldia@naver.com>
2023-03-17 15:22:55 +0900

    FEAT: add Geocode of location in city component

--- src/components/organism/Home.js
+++ src/components/organism/Home.js
@@ -1,9 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import * as Location from 'expo-location';
+
 import City from '../atom/City';
 import DayWeather from '../molecules/DayWeather';
 
 export default function Home() {
+  const [city, setCity] = useState('...Loading');
+  const [location, setLocation] = useState();
+  const [msg, setMsg] = useState(true);
+  const ask = async () => {
+    const permission = await Location.requestForegroundPermissionsAsync();
+    if (permission !== 'granted') {
+      setMsg(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false },
+    );
+    console.log(location[0].city);
+    setCity(city);
+  };
+
+  useEffect(() => {
+    ask();
+  }, []);
+
   return (
     <>
       <View style={styles.container}>
