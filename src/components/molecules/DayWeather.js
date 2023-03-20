da43a6393162e67bc02b7d5d35dcfb1c5f469818
JKKook <whdrnrdldia@naver.com>
2023-03-20 12:32:40 +0900

    DESIGN: add icons & replace lang for kr

--- src/components/molecules/DayWeather.js
+++ src/components/molecules/DayWeather.js
@@ -5,9 +5,24 @@ import {
   Dimensions,
   ActivityIndicator,
 } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
-// const  {width : SCREEN_WIDTH} = Dimensions.get('window')
+//같은 처리 : const  {width : SCREEN_WIDTH} = Dimensions.get('window')
+
+const icons = {
+  Clear: 'md-sunny',
+  Clouds: 'cloudy',
+  Rain: 'rainy',
+  Snow: 'snow',
+};
+
+const tempText = {
+  온흐림: '흐림',
+  튼구름: '구름많음',
+  맑음: '맑음',
+  '실 비': '비',
+};
 
 export default function DayWeather({ days }) {
   console.log(
@@ -23,18 +38,28 @@ export default function DayWeather({ days }) {
       ) : (
         days.map((day, idx) => (
           <View key={idx} style={styles.day}>
-            <Text style={styles.dayTemp}>
-              {parseFloat(day.temp.day).toFixed(1)}℃
-            </Text>
-            <View style={styles.descContainer}>
-              <Text style={styles.weatherDescription}>
-                {day.weather[0].description}
+            <View style={styles.mainContainer}>
+              <Text style={styles.dayTemp}>
+                {parseFloat(day.temp.day).toFixed(0)}℃
               </Text>
-              <Text style={styles.descriptionTemp}>{day.weather[0].main}</Text>
+              <Ionicons
+                name={icons[day.weather[0].main]}
+                size={56}
+                color='black'
+              />
             </View>
-            <Text style={styles.humidity}>
-              습도 : {parseFloat(day.humidity).toFixed(1)}
-            </Text>
+            <View style={styles.descContainer}>
+              <Text style={styles.descriptionTemp}>{day.weather[0].main}</Text>
+              <Text style={styles.weatherDescription}>
+                {tempText[day.weather[0].description]}
+              </Text>
+            </View>
+            <View style={styles.secondDescContainer}>
+              <Text style={styles.humidity}>
+                {parseFloat(day.humidity).toFixed(0)}%
+              </Text>
+              <Ionicons name='water' size={28} color='white' />
+            </View>
           </View>
         ))
       )}
@@ -47,28 +72,40 @@ const styles = StyleSheet.create({
     width: SCREEN_WIDTH,
     alignItems: 'center',
   },
+  mainContainer: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
   descContainer: {
     flexDirection: 'row',
-    padding: 5,
+    width: '55%',
+    marginTop: 10,
+  },
+  secondDescContainer: {
+    flexDirection: 'row',
+    width: '55%',
+    marginTop: 10,
   },
   dayTemp: {
     fontSize: 102,
     fontWeight: '600',
-    marginTop: 50,
   },
   descriptionTemp: {
-    fontSize: 26,
-    fontWeight: '500',
-    color: 'white',
-  },
-  weatherDescription: {
     fontSize: 26,
     fontWeight: '500',
     marginRight: 10,
     color: 'white',
   },
-  humidity: {
-    fontSize: 18,
+  weatherDescription: {
+    fontSize: 26,
     fontWeight: '500',
+    marginRight: 30,
+    color: 'white',
+  },
+  humidity: {
+    fontSize: 20,
+    fontWeight: '500',
+    color: 'white',
+    marginRight: 10,
   },
 });
